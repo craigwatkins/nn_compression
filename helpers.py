75d4576036183415acd80e7b7ca1052c1505a4e5
craig <craigwatkins@rocketmail.com>
2023-12-19 10:01:17 -0800

    Removed unused code

--- helpers.py
+++ helpers.py
@@ -1,69 +1,5 @@
 import numpy as np
 import cv2
-import os
-import pickle
-
-
-def save_as_binary(file_path, file_name, integers):
-    from huffman_compression import HuffmanCoding
-    import padded_binary as pb
-    """
-    Save a list of integers as a binary file using Huffman coding.
-
-    Args:
-        file_path (str): Path to the directory where the file will be saved.
-        file_name (str): Name of the file without extension.
-        integers (list): List of integers to be saved.
-
-    Returns:
-        int: Size of the saved file in bytes.
-    """
-    #integers = [int(x) + 256 for x in integers]  # make sure the values are all positive
-    huff = HuffmanCoding()
-    save_info, encoded_list = huff.compress(integers)
-    full_path = file_path + file_name + '.bin'
-    pb.write_padded_bytes(save_info, full_path)
-    return os.path.getsize(full_path)
-
-def open_binary(file_path, file_name):
-    from huffman_compression import HuffmanCoding
-    import padded_binary as pb
-    """
-    Open a binary file and return a list of integers.
-
-    Args:
-        file_path (str): Path to the directory where the file is located.
-        file_name (str): Name of the file without extension.
-
-    Returns:
-        list: List of integers.
-    """
-    full_path = file_path + file_name + '.bin'
-    binaries = pb.read_padded_bytes(full_path)
-    if binaries is None:
-        print("Error: Could not open file: ", full_path)
-        return None
-    huff = HuffmanCoding()
-    decompressed = huff.decompress_file(binaries)
-    #decoded_list = [int(x) - 256 for x in decoded_list]  # make sure the values are all positive
-    return decompressed
-
-
-def save_hsv(h, s, v, filepath):
-    # clip the values to between 0 and 255
-    v = np.clip(v, 0, 255)
-    # convert to uint8
-    v = v.astype(np.uint8)
-    h = h.astype(np.uint8)
-    s = s.astype(np.uint8)
-    # combine h, s, and v into one image
-    hsv_image = cv2.merge([h, s, v])
-    # convert to uint8
-    hsv_image = hsv_image.astype(np.uint8)
-    # convert hsv to bgr
-    hsv_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-    # save the image
-    save_image(hsv_image, filepath)
 
 
 def save_image(pixels, filepath):
@@ -73,20 +9,13 @@ def save_image(pixels, filepath):
     if not success:
         raise ValueError(f"Could not save image to the path: {filepath}")
 
+
 def save_jpeg(pixels, filepath, quality=90):
     # Use OpenCV's imwrite function to save the image
     success = cv2.imwrite(filepath, pixels, [cv2.IMWRITE_JPEG_QUALITY, quality])
 
     if not success:
         raise ValueError(f"Could not save image to the path: {filepath}")
-
-
-def extract_hsv(filepath):
-    # Open an image and separate it into H, S, V values
-    image = cv2.imread(filepath)
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    H, S, V = cv2.split(hsv_image)
-    return H, S, V
 
 
 def compare_images(filepath1, filepath2):
@@ -126,52 +55,7 @@ def calculate_psnr(original_path, compressed_path):
 
 
 
-def save_pkl(cluster_centers, filename):
-    # save the cluster centers to a pickle file
-    with open(filename, "wb") as f:
-        pickle.dump(cluster_centers, f)
-
-
-def find_closest(sorted_list, target):
-    # binary search for the closest value to target in sorted_list
-    if not sorted_list:
-        return None
-    low, high = 0, len(sorted_list) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if sorted_list[mid] < target:
-            low = mid + 1
-        elif sorted_list[mid] > target:
-            high = mid - 1
-        else:
-            return sorted_list[mid]
-    if low < len(sorted_list) and high >= 0:
-        return sorted_list[low] if abs(sorted_list[low] - target) < abs(sorted_list[high] - target) else sorted_list[high]
-    elif low < len(sorted_list):
-        return sorted_list[low]
-    else:
-        return sorted_list[high]
 
 
 
 
-
-
-def kmeans_elbow(vectors, range_min, range_max, step=1, random_state=42, n_init=10 ):
-    import matplotlib.pyplot as plt
-    from sklearn.cluster import KMeans
-    inertia = []
-    K_range = range(range_min, range_max, step)
-    for k in K_range:
-        kmeans = KMeans(n_clusters=k, random_state=random_state, n_init=n_init)
-        kmeans.fit(vectors)
-        inertia.append(kmeans.inertia_)
-
-    # Step 4: Plot the elbow curve
-    plt.figure(figsize=(8, 4))
-    plt.plot(K_range, inertia, 'o-')
-    plt.title('Elbow Method')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('Inertia')
-    plt.xticks(K_range)
-    plt.show()
--- main.py
+++ main.py
@@ -13,36 +13,31 @@ def make_reference_image(image_path, crop_size=100, crop_offset=0, file_path="im
     save_jpeg(img, file_path+".jpeg", 100)
 
 
-def test():
+def demo():
     crop_size = 450
     crop_offset = 150
     image = "19"
     source_path = f"""images/kodak/kodim{image}.png"""
     save_path = f"""bins/test/{image}.bin"""
     error_threshold = 5
-    #make_reference_image(source_path, 0, 0, crop_image=False)
     make_reference_image(source_path, crop_size, crop_offset)
 
     print("compressing image")
     compressed = NNCompressor()
-
-    start = time.time()
     compressed.compress('images/test/reference.png', save_path, error_threshold)
-    stop = time.time()
-    print("time to compress", stop - start)
     decompressor = NNCompressor()
     decompressor.decompress(save_path)
     # save the regenerated image
-
     save_image(compressed.compressed_values, "images/test/test.png")
     save_image(decompressor.decompressed_values, "images/test/verify.png")
-
-
-if __name__ == "__main__":
-    test()
     psnr_v = calculate_psnr("images/test/reference.png", "images/test/test.png")
     print("vector psnr", psnr_v)
     psnr_j = calculate_psnr("images/test/reference.png", "images/test/reference.jpeg")
     psnr_verify = calculate_psnr("images/test/test.png", "images/test/verify.png")
     print("jpeg psnr", psnr_j)
     print("verify psnr", psnr_verify)
+
+
+if __name__ == "__main__":
+    demo()
+
